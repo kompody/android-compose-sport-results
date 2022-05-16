064b88a5bb3c7e8a16169585fd0266a1510327f2
iuriip <pavlov@arcsinus.ru>
2022-05-16 10:12:07 +0200

    EU-2 Create App

---
+++ app/src/main/java/com/kompody/etnetera/App.kt
@@ -0,0 +1,13 @@
+package com.kompody.etnetera
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
